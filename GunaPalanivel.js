87c94ded418ddb784541bcc3959515c778188a7f
Guna Palanivel <111827797+GunaPalanivel@users.noreply.github.com>
2024-06-25 12:36:40 +0530

    modified skills info

--- GunaPalanivel.js
+++ GunaPalanivel.js
@@ -11,6 +11,8 @@ const developerInfo = {
   toolbox: {
     frontend: ["HTML5", "CSS3", "JavaScript"],
     languages: ["JavaScript"],
+    jsLibrary: ["ReactJS"],
+    backend: ["Node.js"],
     versionControl: ["Git", "GitHub"],
     databases: ["MySQL", "MongoDB"],
     software: ["Visual Studio Code", "MySQL Workbench", "MongoDB Compass", "Git Bash", "GitHub Desktop"]
@@ -18,7 +20,7 @@ const developerInfo = {
   funFacts: {
     favoriteMovie: "The Pursuit of Happyness",
     moviePreference: "Hollywood over Bollywood",
-    knowledgeSources: "books, videos, or conversations"
+    knowledgeSources: ["books", "videos", "conversations"]
   },
   contact: {
     instagram: "https://www.instagram.com/sandy_sag_",
@@ -30,10 +32,11 @@ function generateReadme(userInfo) {
   const { name, country, passion, websiteURL, linksURL, resumeURL, githubURL, linkedinURL, vsCodeConfigURL, toolbox, funFacts, contact } = userInfo;
 
   return `
-# Hello, I'm ${name}! 🌟
+# Hi there 👋, I'm ${name}! 🌟
 
-I'm a 20-year-old developer based in ${country} 🇮🇳, passionate about ${passion}. 💻
+I'm a 21-year-old developer based in ${country} 🇮🇳, and I'm passionate about ${passion}.
 
+- 🌟 As an [ALPHA member of Microsoft MLSA](https://mvp.microsoft.com/en-US/studentambassadors/profile/e9949081-2982-483b-97a7-c0a8baf88937), driving forward tech innovation.
 - 🌐 Check out my [website](${websiteURL}) to learn more about me.
 - 🔗 Explore my [Links](${linksURL}) to find me on various platforms.
 - 📃 View the PDF version of my resume on the drive and navigate through the links [here](${resumeURL}).
@@ -46,6 +49,10 @@ I'm a 20-year-old developer based in ${country} 🇮🇳, passionate about ${pas
 
 **Programming Languages:** \`${toolbox.languages.join("` `")}\`
 
+**JavaScript Library:** \`${toolbox.jsLibrary.join("` `")}\`
+
+**Backend Development:** \`${toolbox.backend.join("` `")}\`
+
 **Version Control:** \`${toolbox.versionControl.join("` `")}\`
 
 **Databases:** \`${toolbox.databases.join("` `")}\`
@@ -56,11 +63,13 @@ I'm a 20-year-old developer based in ${country} 🇮🇳, passionate about ${pas
 
 - 🎬 "${funFacts.favoriteMovie}" is my favorite movie.
 - 🍿 I prefer ${funFacts.moviePreference}.
-- 🧠 I enjoy gaining knowledge from various sources, be it ${funFacts.knowledgeSources}.
+- 🧠 I enjoy gaining knowledge from various sources, be it ${funFacts.knowledgeSources.join(", ")}.
 
 ## 📫 Contact
 
 For a quick response, DM me on [Instagram](${contact.instagram}) or [LinkedIn](${contact.linkedin}).
+
+<img src="https://media.giphy.com/media/LnQjpWaON8nhr21vNW/giphy.gif" width="60"> <em><b>Let's connect and chat!</b></em>
 `;
 }
 
